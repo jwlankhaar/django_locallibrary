6da7e19634046fd434139ecc6fb5de4617c50c36
jwlankhaar-hu <jan-willem.lankhaar@hu.nl>
2018-06-05 12:46:19 +0200

    Add Heroku Procfile

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h1%pcc@0^cjbi1k&58mtmvppb*a*8t!+lr93k8&--k0#no2sw9'
+SECRET_KEY=os.environ.get('DJANGO_SECRET_KEY',
+    'h1%pcc@0^cjbi1k&58mtmvppb*a*8t!+lr93k8&--k0#no2sw9')
+# SECRET_KEY = 'h1%pcc@0^cjbi1k&58mtmvppb*a*8t!+lr93k8&--k0#no2sw9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
